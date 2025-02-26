265d4384f97be8193e15881f4c9772ed9a0f8ad0
LukeParton <luke.parton@moneyinfo.com>
2025-02-26 17:23:36 +0000

    added test conditions

--- src/classes/__tests__/recipe.test.ts
+++ src/classes/__tests__/recipe.test.ts
@@ -1,6 +1,424 @@
+import {describe, it} from "vitest"
 
 
+describe("Recipe", () => {
+    describe("checkFitsValuesMeat", () => {
+        it("returns true if max and min are undefined", () => {
 
-it("TEST", () => {
-    expect(true).toBe(true)
+        })
+
+        describe("min", () => {
+            it("returns true if summed vals greater than min", () => {
+
+            })
+
+            it("returns true if summed value equal to min", () => {
+
+            })
+
+            it("returns false if summed value lower than min", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+
+        describe("max", () => {
+            it("returns true if summed vals lower than max", () => {
+
+            })
+
+            it("returns true if summed value equal to max", () => {
+
+            })
+
+            it("returns false if summed value greater than max", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+    })
+
+    describe("checkFitsValuesFish", () => {
+        it("returns true if max and min are undefined", () => {
+
+        })
+
+        describe("min", () => {
+            it("returns true if summed vals greater than min", () => {
+
+            })
+
+            it("returns true if summed value equal to min", () => {
+
+            })
+
+            it("returns false if summed value lower than min", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+
+        describe("max", () => {
+            it("returns true if summed vals lower than max", () => {
+
+            })
+
+            it("returns true if summed value equal to max", () => {
+
+            })
+
+            it("returns false if summed value greater than max", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+    })
+
+    describe("checkFitsValuesEgg", () => {
+        it("returns true if max and min are undefined", () => {
+
+        })
+
+        describe("min", () => {
+            it("returns true if summed vals greater than min", () => {
+
+            })
+
+            it("returns true if summed value equal to min", () => {
+
+            })
+
+            it("returns false if summed value lower than min", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+
+        describe("max", () => {
+            it("returns true if summed vals lower than max", () => {
+
+            })
+
+            it("returns true if summed value equal to max", () => {
+
+            })
+
+            it("returns false if summed value greater than max", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+    })
+
+    describe("checkFitsValuesFruit", () => {
+        it("returns true if max and min are undefined", () => {
+
+        })
+
+        describe("min", () => {
+            it("returns true if summed vals greater than min", () => {
+
+            })
+
+            it("returns true if summed value equal to min", () => {
+
+            })
+
+            it("returns false if summed value lower than min", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+
+        describe("max", () => {
+            it("returns true if summed vals lower than max", () => {
+
+            })
+
+            it("returns true if summed value equal to max", () => {
+
+            })
+
+            it("returns false if summed value greater than max", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+    })
+
+    describe("checkFitsValuesVeg", () => {
+        it("returns true if max and min are undefined", () => {
+
+        })
+
+        describe("min", () => {
+            it("returns true if summed vals greater than min", () => {
+
+            })
+
+            it("returns true if summed value equal to min", () => {
+
+            })
+
+            it("returns false if summed value lower than min", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+
+        describe("max", () => {
+            it("returns true if summed vals lower than max", () => {
+
+            })
+
+            it("returns true if summed value equal to max", () => {
+
+            })
+
+            it("returns false if summed value greater than max", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+    })
+
+    describe("checkFitsValuesSweetener", () => {
+        it("returns true if max and min are undefined", () => {
+
+        })
+
+        describe("min", () => {
+            it("returns true if summed vals greater than min", () => {
+
+            })
+
+            it("returns true if summed value equal to min", () => {
+
+            })
+
+            it("returns false if summed value lower than min", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+
+        describe("max", () => {
+            it("returns true if summed vals lower than max", () => {
+
+            })
+
+            it("returns true if summed value equal to max", () => {
+
+            })
+
+            it("returns false if summed value greater than max", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+    })
+
+    describe("checkFitsValuesMonster", () => {
+        it("returns true if max and min are undefined", () => {
+
+        })
+
+        describe("min", () => {
+            it("returns true if summed vals greater than min", () => {
+
+            })
+
+            it("returns true if summed value equal to min", () => {
+
+            })
+
+            it("returns false if summed value lower than min", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+
+        describe("max", () => {
+            it("returns true if summed vals lower than max", () => {
+
+            })
+
+            it("returns true if summed value equal to max", () => {
+
+            })
+
+            it("returns false if summed value greater than max", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+    })
+
+    describe("checkFitsValuesDairy", () => {
+        it("returns true if max and min are undefined", () => {
+
+        })
+
+        describe("min", () => {
+            it("returns true if summed vals greater than min", () => {
+
+            })
+
+            it("returns true if summed value equal to min", () => {
+
+            })
+
+            it("returns false if summed value lower than min", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+
+        describe("max", () => {
+            it("returns true if summed vals lower than max", () => {
+
+            })
+
+            it("returns true if summed value equal to max", () => {
+
+            })
+
+            it("returns false if summed value greater than max", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+    })
+
+    describe("checkFitsValuesBug", () => {
+        it("returns true if max and min are undefined", () => {
+
+        })
+
+        describe("min", () => {
+            it("returns true if summed vals greater than min", () => {
+
+            })
+
+            it("returns true if summed value equal to min", () => {
+
+            })
+
+            it("returns false if summed value lower than min", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+
+        describe("max", () => {
+            it("returns true if summed vals lower than max", () => {
+
+            })
+
+            it("returns true if summed value equal to max", () => {
+
+            })
+
+            it("returns false if summed value greater than max", () => {
+
+            })
+
+            it("removes required ingredients from the calculation", () => {
+
+            })
+        })
+    })
+
+    describe("checkFitsRestrictedIngredients", () => {
+        it("returns false if has ingredient which is restricted", () => {
+
+        })
+
+        it("returns true if does not have ingredient which is restricted", () => {
+
+        })
+    })
+
+    describe("checkFitsRequiredIngredients", () => {
+        it("returns true if has an ingredient matching an ingredient in each array", () => {
+
+        })
+
+        it("returns false if required ingredients has an array without a matching ingredient", () => {
+
+        })
+    })
+
+    describe("checkFitsInedible", () => {
+        it("returns true when allowInedible is true", () => {
+
+        })
+
+        it("returns true when allowInedible is false but no inedible ingredients given", () => {
+
+        })
+
+        it("returns false when allowInedible is false and inedible ingredient given", () => {
+
+        })
+
+        it("returns true when allowInedible is false and inedible ingredient given but it is required", () => {
+
+        })
+
+        it("returns false when allowInedible is false when inedible ingredient required but it is given twice", () => {
+            
+        })
+    })
 })
